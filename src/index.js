32bbd3df356006a3987c5e41d2ac18c409a92ff9
Jon <jon.walz.music@gmail.com>
2019-05-26 13:11:14 -0700

    updated server endpoints

--- src/index.js
+++ src/index.js
@@ -1,39 +1,68 @@
-"use strict";
+'use strict'
 
-const Hapi = require("@hapi/hapi");
-const axios = require("axios");
+const Hapi = require('@hapi/hapi')
+const axios = require('axios')
 
-const url = "https://teamtreehouse.com/jonathanwalz.json";
+const url = 'https://teamtreehouse.com/jonathanwalz.json'
 
 async function getUser(req, h) {
-  try {
-    const data = await axios.get(url);
-    console.log(data);
-    return h.response(data.data);
-  } catch (err) {
-    return h.err(err);
-  }
+    try {
+        const resp = await axios.get(url)
+        return h
+            .response(
+                JSON.stringify({
+                    data: resp.data.points,
+                }),
+            )
+            .code(200)
+            .header('access-control-allow-methods', 'GET', 'OPTIONS')
+            .header('content-type', 'application/json')
+    } catch (err) {
+        return h.err(err)
+    }
 }
 
 const init = async () => {
-  const server = Hapi.server({
-    port: 8080,
-    host: "localhost"
-  });
+    const server = Hapi.server({
+        port: 8080,
+        host: 'localhost',
+    })
 
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: getUser
-  });
+    server.route({
+        method: 'OPTIONS',
+        path: '/points',
+        handler: (req, h) => {
+            return h
+                .response('success')
+                .code(200)
+                .header('Access-Control-Allow-Origin', '*')
+                .header('Access-Control-Allow-Headers', 'content-type')
+                .header('Access-Control-Allow-Methods', 'GET', 'OPTIONS')
+        },
+    })
 
-  await server.start();
-  console.log("Server running on %ss", server.info.uri);
-};
+    server.route({
+        method: 'GET',
+        path: '/points',
+        handler: getUser,
+        options: {
+            json: {
+                space: 4,
+                suffix: 'after',
+            },
+            cors: {
+                origin: [ '*' ],
+            },
+        },
+    })
 
-process.on("unhandledRejection", err => {
-  console.log(err);
-  process.exit(1);
-});
+    await server.start()
+    console.log('Server running on %ss', server.info.uri)
+}
 
-init();
+process.on('unhandledRejection', (err) => {
+    console.log(err)
+    process.exit(1)
+})
+
+init()
